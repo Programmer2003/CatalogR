81a0ede1fa41408fa3400ea9e7272ea39b1c28a2
Christian Repnik <kris18star@gmail.com>
2024-03-03 10:45:10 +0300

    Collections Update + Icons to actions

--- Controllers/UserCollectionsController.cs
+++ Controllers/UserCollectionsController.cs
@@ -57,5 +57,61 @@ namespace CatalogR.Controllers
             ViewData["Topics"] = new SelectList(_context.CollectionTopics, "Id", "Name", collection.CollectionTopicId);
             return View(collection);
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Collections == null)
+            {
+                return NotFound();
+            }
+
+            var collection = await _context.Collections.FindAsync(id);
+            if (collection == null)
+            {
+                return NotFound();
+            }
+
+            ViewData["Topics"] = new SelectList(_context.CollectionTopics, "Id", "Name", collection.CollectionTopicId);
+            return View(collection);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,UserId,CollectionTopicId")] Collection collection)
+        {
+            if (id != collection.Id || collection.UserId != _userManager.GetUserId(User))
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(collection);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CollectionExists(collection.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewData["Topics"] = new SelectList(_context.CollectionTopics, "Id", "Name", collection.CollectionTopicId);
+            return View(collection);
+        }
+
+        private bool CollectionExists(int id)
+        {
+            return (_context.Collections?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
     }
 }
