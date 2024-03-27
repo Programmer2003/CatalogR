f3e8e1abf2463e081299ccb787d80c9b9500bc24
Christian Repnik <kris18star@gmail.com>
2024-03-27 12:04:49 +0300

    Admin Controller. (Un)Block Users. Delete Users

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,51 @@
+﻿using CatalogR.Data;
+using CatalogR.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace CatalogR.Controllers
+{
+    [Authorize(Roles = "admin,Admin")]
+    public class AdminController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AdminController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var users = await _context.Users.AsNoTracking().ToListAsync();
+            return View(users);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> UsersChangeBlockStatus(string data, bool block)
+        {
+            var list = JsonConvert.DeserializeObject<List<string>>(data);
+            if (list == null) return Json(HttpStatusCode.OK);
+
+            var users = await _context.Users.Where(u => list.Contains(u.Id)).ToListAsync();
+            users.ForEach(u => u.IsLocked = block);
+            await _context.SaveChangesAsync();
+            return Json(HttpStatusCode.OK);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> DeleteUsers(string data)
+        {
+            var list = JsonConvert.DeserializeObject<List<string>>(data);
+            if (list == null) return Json(HttpStatusCode.OK);
+
+            _context.Comments.RemoveRange(_context.Comments.Where(c => list.Contains(c.UserId!)));
+            _context.Users.RemoveRange(_context.Users.Where(u => list.Contains(u.Id)));
+            await _context.SaveChangesAsync();
+            return Json(HttpStatusCode.OK);
+        }
+    }
+}
